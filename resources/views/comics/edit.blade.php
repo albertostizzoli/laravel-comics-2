0065b8821934ffc9268ad6bb69b137eb0873741c
albertostizzoli <albertostizzoli@yahoo.it>
2024-01-10 16:53:24 +0100

    added error on edit page

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -13,29 +13,44 @@
                     @method('PUT')
                     <div class="mb-3">
                         <label for="title" class="form-label">Title</label>
-                        <input type="text" class="form-control" id="title" value="{{ old('title', $comic->title) }}"
+                        <input type="text" class="form-control  @error('title') is-invalid @enderror" id="title" value="{{ old('title', $comic->title) }}"
                             placeholder="Insert a Title" name="title">
+                            @error('title')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="price" class="form-label">Price</label>
-                        <input type="text" class="form-control" id="price" value="{{ old('price', $comic->price) }}"
+                        <input type="text" class="form-control  @error('price') is-invalid @enderror" id="price" value="{{ old('price', $comic->price) }}"
                             placeholder="Insert a Price" name="price">
+                            @error('price')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="type" class="form-label">Type</label>
-                        <input type="text" class="form-control" id="type" value="{{ old('type', $comic->type) }}"
+                        <input type="text" class="form-control  @error('type') is-invalid @enderror" id="type" value="{{ old('type', $comic->type) }}"
                             placeholder="Insert a Type" name="type">
+                            @error('type')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="series" class="form-label">Series</label>
-                        <input type="text" class="form-control" id="series"
+                        <input type="text" class="form-control  @error('series') is-invalid @enderror" id="series"
                             value="{{ old('series', $comic->series) }}" placeholder="Insert a Series" name="series">
+                            @error('series')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="sale_date" class="form-label">Sale Date</label>
-                        <input type="text" class="form-control" id="sale_date"
+                        <input type="text" class="form-control  @error('sale_date') is-invalid @enderror" id="sale_date"
                             value="{{ old('sale_date', $comic->sale_date) }}" placeholder="Insert a Sale Date"
                             name="sale_date">
+                            @error('sale_date')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="thumb" class="form-label">Image</label>
